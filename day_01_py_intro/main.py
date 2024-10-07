ecd870ce4ee0429dfb1da6068c1efe27c3049953
Emmanuel Chukwunonso Ighavbota <emmanuelighavbota@gmail.com>
2024-10-07 14:24:27 +0100

    Finished day 1 tasks and project

---
+++ day_01_py_intro/main.py
@@ -0,0 +1,27 @@
+"""Day 1 used the default editor on udemy for accessing and debugging the exercises.
+It focuses on print function and debugging, string concatenation and manipulation"""
+
+# Fixed the code below 👇
+
+# print("Notes from Day 1")
+# print("The print statement is used to output strings")
+# print("Strings are strings of characters")
+# print("String Concatenation is done with the + sign")
+# print("New lines can be created with a \ and the letter n")
+
+# Write 3 lines of code to switch the contents of the variables.
+# You are not allowed to type the words "milk" or
+# "juice". You are only allowed to use variables to solve this exercise.
+
+# glass1 = "milk"
+# glass2 = "juice"
+#
+# glass = glass2
+# glass2 = glass1
+# glass1 = glass
+
+"""Final Project: Band name generator"""
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your Pet's name?\n")
+print(f"Your band name should be {pet_name} {city}.")

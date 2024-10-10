db800b9f795f2861dcfd811587bdb0b34058525f
Emmanuel Chukwunonso Ighavbota <emmanuelighavbota@gmail.com>
2024-10-10 16:35:38 +0100

    Added the hurdles exercises solution

---
+++ day_06_reeborgs_world_exercises/main.py
@@ -0,0 +1,26 @@
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# def move_past_hurdle():
+#     turn_left()
+#
+#     while not right_is_clear():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+#
+#
+# while not at_goal():
+#     if front_is_clear():
+#         move()
+#     else:
+#         move_past_hurdle()
+#
+#
